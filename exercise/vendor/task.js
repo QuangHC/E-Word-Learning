458f84317b58fc13e4e834e579821f7f1edefec7
Luo1604 <98147788+Luo1604@users.noreply.github.com>
2023-07-26 01:20:14 +0700

    task

--- exercise/vendor/task.js
+++ exercise/vendor/task.js
@@ -1,8 +1,7 @@
 $(function () {
     let selectedData_0 = null;
-    let remainingItems_0 = $('#main_list_0 li').length;
+    let remainingItems_0 = document.getElementsByClassName("question").length;
     $('#remainingText_0').text(`${remainingItems_0} items remaining`);
-   
 
     // Function để lấy giá trị của data-question-index và nội dung của thẻ li khi được click
     function getQuestionIndexAndText(item) {
@@ -68,61 +67,8 @@ $(function () {
             }
         }
 
-        $('#remainingText_0').text(`${countRemainingItems()} items remaining`);
-        if (countRemainingItems() < remainingItems_0) {
-            $('.action .btn.btn-success').removeClass('disabled');
-        } else {
-            $('.action .btn.btn-success').addClass('disabled');
-        }
-        if (countRemainingItems() === 0) {
-            $('#remainingText_0').text('All items have been filled');
-        }
+        
+        
     });
-
-    // $('[data-bs-target="#modal"]').on('click', function () {
-    //     $('#modal .modal-body p').text(`Answered questions: ${remainingItems_0 - countRemainingItems()} out of ${remainingItems_0}. Do you want to finish?`);
-    //     $('ul#main_list_0 li').addClass('disable');
-    // });
-
-    // $('[data-bs-target="#modal0"]').on('click', function () {
-    //     const totalCorrectAnswers = countCorrectAnswers();
-    //     const percentage = (totalCorrectAnswers / remainingItems_0 * 100).toFixed(2); // Lấy 2 chữ số sau dấu thập phân
-    //     $('#modal0 .modal-body p').text(`Total score: ${totalCorrectAnswers} out of ${remainingItems_0} (${percentage}%)`);
-    //     $('li.list-group-item.btn.btn-light.incorrect').css('background-color', '#FAC8C1    ');
-    //     $('li.list-group-item.btn.btn-light.correct').css('background-color', '#B7E9BF');
-    //     $('i.hidden').removeClass('hidden');
-    //     $('.action .btn.btn-danger').removeClass('disabled');
-    //     $('.action .btn.btn-success').addClass('disabled');
-    //     $('#answer-task li.list-group-item.btn.btn-light').addClass('disable');
-    //     $('#close0').click();
-    // }); 
-    
-    // Dem so luong item con lai trong main_list khong co thuoc tinh hidden
-    function countRemainingItems() {
-        const hiddenItem = $('#main_list_0 li.list-group-item.btn.hidden').length;
-        console.log(hiddenItem);
-        return remainingItems_0 - hiddenItem;
-    }
-
-    $('.action .btn.btn-danger').on('click', function () {
-        reset();
-        if ($('span.answer').not('.hidden')) {
-            $('.action .btn.btn-info').click();
-        }
-        $('#answer-task li.list-group-item.btn.btn-light').removeClass('disable');
-        $('.action .btn.btn-danger').addClass('disabled');
-        $('.gapfield-list-0 li').text('').data('question-index', '-1').removeClass('correct incorrect');
-        $('#answer-task li.list-group-item.btn.btn-light').css('background-color', '#FFFFFF');
-        $('ul#main_list_0 li').removeClass('disable');
-        $('.check-icon').removeClass('fa-check fa-xmark');
-    });
-
-    function reset() {
-        $('#main_list_0 li').removeClass('hidden');
-        $('#remainingText_0').text(`${remainingItems_0} items remaining`);
-        $('i.hidden').removeClass('hidden');
-        $('.action .btn.btn-success').addClass('disabled');
-        $('.check-icon').addClass('hidden');
-    };
 
 });
