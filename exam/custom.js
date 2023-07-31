b7064ba570262dfb2445d949203d8f62a21b9e11
Luo1604 <98147788+Luo1604@users.noreply.github.com>
2023-07-31 23:07:55 +0700

    test (exam)

--- exam/custom.js
+++ exam/custom.js
@@ -1,15 +1,15 @@
 $(function () {
     var countDownDate = new Date().getTime() + 30 * 60 * 1000; // Thời gian kết thúc đếm ngược là 30 phút sau thời điểm hiện tại
     var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    $("#time").text("Time remaining: " + minutes + ":" + seconds);
-    if (distance < 0) {
-      clearInterval(x);
-      $("#time").text("End Time");
-    }
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        $("#time").text("Time remaining: " + minutes + ":" + seconds);
+        if (distance < 0) {
+          clearInterval(x);
+          $("#time").text("End Time");
+      }
   }, 1000);
 
     // begin: Task 1
@@ -18,21 +18,47 @@ $(function () {
     let task_selectedData = null;
     changeRemainingQuestions();
 
-     $(".rq-Question-answerList li").click(function() {
+    $('#btn-next').on('click', function () {
+        question_index_current++;
+        console.log("qic: " + question_index_current);
+        $(`#question-${question_index_current - 1}.Quiz-inner`).css("display", "none");
+        $(`#question-${question_index_current}.Quiz-inner`).css("display", "block");
+        ableButton(question_index_current);
+        task_selectedData = null;
+        // $('.selecting-answer').removeClass('selecting-answer');
+        // $('.selecting-btn').removeClass('selecting-btn');
+        changeRemainingQuestions();
+    });
+
+
+    $('#btn-prev').addClass('disabled');
+    $('#btn-next').addClass('disabled');
+
+    $('#btn-prev').on('click', function () {
+        question_index_current--;
+        console.log("qic: " + question_index_current);
+        $(`#question-${question_index_current + 1}.Quiz-inner`).css("display", "none");
+        $(`#question-${question_index_current}.Quiz-inner`).css("display", "block");
+        ableButton(question_index_current);
+        task_selectedData = null;
+        // $('.selecting-answer').removeClass('selecting-answer');
+        // $('.selecting-btn').removeClass('selecting-btn');
+        changeRemainingQuestions();
+    });
+
+    $(" .radio label").click(function() {
         if ($(this).hasClass('selecting-answer')) {
             $(this).removeClass('selecting-answer');
-            $(this).css('background-color', '#fff');
         } else {
             task_selectedData = $(this);
             $('.selecting-answer').removeClass('selecting-answer');
             $(this).addClass('selecting-answer');
-            $(this).css('background-color', '#DEDEDE');
         }
         changeRemainingQuestions();
 
-  });
+    });
     
-   $(".Quiz-confidence-buttons a").click(function() {
+    $(".Quiz-confidence-buttons a").click(function() {
         if ($(this).hasClass('selecting-btn')) {
             $(this).removeClass('selecting-btn');
             $(this).css('background-color', '#fff');
@@ -43,39 +69,9 @@ $(function () {
         }
         changeRemainingQuestions();
 
-  });
-
-
-    $('#btn-next').on('click', function () {
-        question_index_current++;
-        console.log("qic: " + question_index_current);
-        $(`#question-${question_index_current - 1}.Quiz-inner`).css("display", "none");
-        $(`#question-${question_index_current}.Quiz-inner`).css("display", "block");
-        ableButton(question_index_current);
-        task_selectedData = null;
-        $('.selecting-answer').removeClass('selecting-answer');
-        $('.selecting-btn').removeClass('selecting-btn');
-        changeRemainingQuestions();
     });
 
 
-    $('#btn-prev').addClass('disabled');
-    // $('#btn-prev').addClass('disable');
-
-    $('#btn-prev').on('click', function () {
-        
-        question_index_current--;
-        console.log("qic: " + question_index_current);
-        $(`#question-${question_index_current + 1}.Quiz-inner`).css("display", "none");
-        $(`#question-${question_index_current}.Quiz-inner`).css("display", "block");
-        ableButton(question_index_current);
-        task_selectedData = null;
-        $('.selecting-answer').removeClass('selecting-answer');
-        $('.selecting-btn').removeClass('selecting-btn');
-        changeRemainingQuestions();
-    });
-
-      
     function ableButton(index) {
         if (index === 1) {
             $('#btn-prev').addClass('disabled');
@@ -88,13 +84,18 @@ $(function () {
     }
 
     function filledAll(question_index_current) {
-        let l = $(`#question-${question_index_current} .rq-Question-answerList li.answer.selecting-answer`).length;
+        let l = $(`#question-${question_index_current} .radio label.selecting-answer`).length;
         let h = $(`#question-${question_index_current} .Quiz-confidence-buttons a.btn.btn-default.selecting-btn`).length;
         // kiem tra so luong cau hoi con lai trong tung question
+        console.log(l);
+        console.log(h);
         if (l === 1 && h === 1) {
             $(`#question-${question_index_current}`).addClass('filled-all');
+            $('#btn-next').removeClass('disabled');
         } else { 
             $(`#question-${question_index_current}`).removeClass('filled-all');
+            $('#btn-next').addClass('disabled');
+
         }
     }
 
